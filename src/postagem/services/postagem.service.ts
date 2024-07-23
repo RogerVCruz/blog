4f94e8c10bed9ebdc7a9948ac74928a3f1e1a7b6
Roger Vinicius <rogervtsc@hotmail.com>
2024-07-23 12:27:46 -0300

    CRUD

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -28,5 +28,41 @@ export class PostagemService {
       );
 
     return buscaPostagem;
+  }
+
+  async findByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`),
+      },
+    });
+  }
+
+  async create(postagem: Postagem): Promise<Postagem> {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem): Promise<Postagem> {
+    const buscaPostagem = await this.findById(postagem.id);
+
+    if (!buscaPostagem || !postagem.id)
+      throw new HttpException(
+        'A Postagem não foi encontrada',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    const buscaPostagem = await this.findById(id);
+
+    if (!buscaPostagem)
+      throw new HttpException(
+        'A Postagem não foi encontrada!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return await this.postagemRepository.delete(id);
   }
 }
