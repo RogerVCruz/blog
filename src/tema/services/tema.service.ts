df5d678bc5cc29b81975a66372eb5b2c5697389b
Roger Vinicius <rogervtsc@hotmail.com>
2024-07-24 10:30:48 -0300

    CRUD Tema

---
+++ src/tema/services/tema.service.ts
@@ -0,0 +1,68 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tema } from '../entities/tema.entity';
+
+@Injectable()
+export class TemaService {
+  constructor(
+    @InjectRepository(Tema)
+    private temaRepository: Repository<Tema>,
+  ) {}
+
+  async findAll(): Promise<Tema[]> {
+    return await this.temaRepository.find();
+  }
+
+  async findById(id: number): Promise<Tema> {
+    const buscaTema = await this.temaRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!buscaTema)
+      throw new HttpException(
+        'O Tema não foi encontrado!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return buscaTema;
+  }
+
+  async findByTitulo(titulo: string): Promise<Tema[]> {
+    return await this.temaRepository.find({
+      where: {
+        descricao: ILike(`%${titulo}%`),
+      },
+    });
+  }
+
+  async create(tema: Tema): Promise<Tema> {
+    return await this.temaRepository.save(tema);
+  }
+
+  async update(tema: Tema): Promise<Tema> {
+    const buscaTema = await this.findById(tema.id);
+
+    if (!buscaTema || !tema.id)
+      throw new HttpException(
+        'O Tema não foi encontrado!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return await this.temaRepository.save(tema);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    const buscaTema = await this.findById(id);
+
+    if (!buscaTema)
+      throw new HttpException(
+        'O Tema não foi encontrado!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return await this.temaRepository.delete(id);
+  }
+}
