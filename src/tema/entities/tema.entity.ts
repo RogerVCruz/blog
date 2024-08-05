e72bece52ad079ab82415eb090583587b060d483
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-05 15:14:35 -0300

    feat: add swagger-ui docs

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -2,17 +2,21 @@ import { Transform, TransformFnParams } from 'class-transformer';
 import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Postagem } from '../../postagem/entities/postagem.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_temas' })
 export class Tema {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @IsNotEmpty()
+  @ApiProperty()
   @Column({ length: 100, nullable: false })
   descricao: string;
 
   @OneToMany(() => Postagem, (postagem) => postagem.tema)
+  @ApiProperty()
   postagem: Postagem[];
 }
